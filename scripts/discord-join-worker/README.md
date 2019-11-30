71cc729fc168ce392a2fae9098de01cb39b5dd8a
Hrishikesh Barman <hrishikeshbman@gmail.com>
2019-11-30 05:55:19 +0530

    wip

--- scripts/discord-join-worker/README.md
+++ scripts/discord-join-worker/README.md
@@ -4,15 +4,16 @@
 
 ---
 
-This worker takes in `POST` request with json body `{username,email}` and sends a discord invitation email to the given email and adds the email and username to [fossassam-public-members]( https://github.com/buildandtell/fossassam-public-members)
+This worker takes in `POST` request with json body `{username,email}` and sends a discord invitation email to the given email and adds the email and username to [fossassam-public-members](https://github.com/buildandtell/fossassam-public-members)
 
 Replace these two lines with actual api keys.
+
 ```
 const sgToken = "<API_TOKEN>"
 const ghToken = "<API_TOKEN>"
 ```
 
 ## Instructions
+
 - Install the [wrangler cli](https://github.com/cloudflare/wrangler)
 - Run `wrangler preview`
-
--- scripts/discord-join-worker/index.js
+++ scripts/discord-join-worker/index.js
@@ -3,14 +3,17 @@ addEventListener('fetch', event => {
 })
 
 const commonHeaders = { 'content-type': 'application/json;charset=UTF-8' }
-const sgToken = "<API_TOKEN>"
-const ghToken = "<API_TOKEN>"
+const sgToken = '<API_TOKEN>'
+const ghToken = '<API_TOKEN>'
 
-function addcors(r)  {
+function addcors(r) {
   r.headers.set('Access-Control-Allow-Origin', '*')
-  r.headers.set("Access-Control-Allow-Methods", "POST","OPTIONS")
+  r.headers.set('Access-Control-Allow-Methods', 'POST', 'OPTIONS')
   r.headers.set('Vary', 'Origin')
-  r.headers.set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+  r.headers.set(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+  )
   return r
 }
 
@@ -23,13 +26,15 @@ async function handleRequest(request) {
   let response
   // Add CORS headers
 
-
   if (request.method === 'POST') {
     const body = await request.json()
     // validate body
-    if (body.username == undefined || body.email == undefined ) {
-      let err = { err: "Bad request" }
-      response = new Response(JSON.stringify(err), { status: 400, headers: commonHeaders})
+    if (body.username == undefined || body.email == undefined) {
+      let err = { err: 'Bad request' }
+      response = new Response(JSON.stringify(err), {
+        status: 400,
+        headers: commonHeaders,
+      })
       return addcors(response)
     }
 
@@ -38,48 +43,63 @@ async function handleRequest(request) {
       await makeRepoDispatch(body)
       // SendGrid email
       await sendMail(body)
-    } catch(e) {
-      response = new Response(JSON.stringify(`{"err":"${e.message}"}`), { status: 500, headers: commonHeaders})
+    } catch (e) {
+      response = new Response(JSON.stringify(`{"err":"${e.message}"}`), {
+        status: 500,
+        headers: commonHeaders,
+      })
       return addcors(response)
     }
     // successful return
-    response = new Response('{"body":"Registered."}', { status: 201, headers: commonHeaders})
+    response = new Response('{"body":"Registered."}', {
+      status: 201,
+      headers: commonHeaders,
+    })
   } else if (request.method === 'OPTIONS') {
-    response = new Response(JSON.stringify(["OK"]), { status: 200, headers: commonHeaders})
+    response = new Response(JSON.stringify(['OK']), {
+      status: 200,
+      headers: commonHeaders,
+    })
   } else {
-    let err = { err: "Expected POST" }
-    response = new Response(JSON.stringify(err), { status: 405, headers: commonHeaders})
+    let err = { err: 'Expected POST' }
+    response = new Response(JSON.stringify(err), {
+      status: 405,
+      headers: commonHeaders,
+    })
   }
 
   return addcors(response)
 }
 
 async function makeRepoDispatch(requestBody) {
-  const ghRepo = "buildandtell/fossassam-public-members"
-  const rawResponse = await fetch(`https://api.github.com/repos/${ghRepo}/dispatches`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/vnd.github.everest-preview+json',
-      'Content-Type': 'application/json',
-      'User-Agent': 'fossassam-discord-worker',
-      'Authorization': `token ${ghToken}`
+  const ghRepo = 'buildandtell/fossassam-public-members'
+  const rawResponse = await fetch(
+    `https://api.github.com/repos/${ghRepo}/dispatches`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/vnd.github.everest-preview+json',
+        'Content-Type': 'application/json',
+        'User-Agent': 'fossassam-discord-worker',
+        Authorization: `token ${ghToken}`,
+      },
+      body: JSON.stringify({
+        event_type: 'add_user',
+        client_payload: {
+          username: requestBody.username,
+          email: requestBody.email,
+        },
+      }),
     },
-    body: JSON.stringify({
-     event_type: "add_user",
-     client_payload: {
-       "username": requestBody.username,
-       "email": requestBody.email
-     }
-    })
-  })
-  if (rawResponse.status !== 204){
-    throw "repository-dispatch event was not created"
+  )
+  if (rawResponse.status !== 204) {
+    throw 'repository-dispatch event was not created'
   }
 }
 
 async function sendMail(requestBody) {
-  const apiUrl = "https://api.sendgrid.com/v3/mail/send"
-  const subjectLine = "FossAssam Discord Group Invitation 🎉"
+  const apiUrl = 'https://api.sendgrid.com/v3/mail/send'
+  const subjectLine = 'FossAssam Discord Group Invitation 🎉'
   const contentBody = `Hello @${requestBody.username}!
 
 Here's your invitation link to the discord group, we would suggest downloading the mobile phone app for discord aswell.
@@ -91,20 +111,18 @@ Welcome you to a happy and friendly family! 🤩`
   const rawResponse = await fetch(apiUrl, {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${sgToken}`
+      Authorization: `Bearer ${sgToken}`,
     },
-    body: JSON.stringify(
-      {
-        "personalizations": [{"to": [{"email": requestBody.email}]}],
-        "from": {"email": "fossassam@gmail.com"},
-        "subject": subjectLine,
-        "content": [{"type": "text/plain", "value": contentBody}],
-      }
-    )
+    body: JSON.stringify({
+      personalizations: [{ to: [{ email: requestBody.email }] }],
+      from: { email: 'fossassam@gmail.com' },
+      subject: subjectLine,
+      content: [{ type: 'text/plain', value: contentBody }],
+    }),
   })
-  if (rawResponse.status !== 202){
-    throw "email could not be sent"
+  if (rawResponse.status !== 202) {
+    throw 'email could not be sent'
   }
 }
